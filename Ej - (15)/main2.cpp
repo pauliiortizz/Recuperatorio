6e961d250ed7aed496b2fc0d526f4be98d7b68f7
pauliiortizz <127853900+pauliiortizz@users.noreply.github.com>
2023-12-09 12:01:02 -0300

    Update main2.cpp

--- Ej - (15)/main2.cpp
+++ Ej - (15)/main2.cpp
@@ -1,42 +1,62 @@
 #include <iostream>
 #include "Cola.h"
-
 using namespace std;
 
-template <class T>
-void ParImpar(Pila<T>& pila, Cola<T>& cola, int n) {
-    for (int i = 0; i < n; i++) {
-        if (cola.peek() % 2 == 0) {
-            cola.encolar(cola.peek());
-        } else {
-            pila.push(cola.peek());
-        }
-        cola.desencolar();
+/*
+ * Implementar una función Reemplazar que tenga como argumentos una pila de enteros y dos valores enteros:
+ * nuevo y viejo, de forma que si viejo aparece en algún lugar de la pila, sea reemplazado por nuevo.
+ */
+
+Pila<int> reemplazar(Pila<int> pila, int nuevo, int viejo) {
+    int aux;
+    while (pila.esVacia()){
+        throw 404;
     }
+
+    Pila<int> pilaaux;
+
+    while (!pila.esVacia()){      //
+        aux=pila.pop();
+        if (aux==viejo){
+            pilaaux.push(nuevo);
+        }
+        else {
+            pilaaux.push(aux);
+        }
+    }
+
+    return pilaaux;
 }
 
 int main(){
-    Cola<int> cola;
     Pila<int> pila;
 
     int n;
     int valor;
-    cout << "Ingrese el tamano de la cola: ";
+    cout << "Ingrese el tamano de la pila: ";
     cin >> n;
 
     for (int i=0; i<n; i++){
         cout << "Elemento " << i << ": ";
         cin >> valor;
-        cola.encolar(valor);
+        pila.push(valor);
     }
 
+    int nuevo, viejo;
 
-    cout << "Cola original: ";
-    cola.imprimir();
-    cout << "Cola modificada: ";
-    ParImpar(pila, cola, n);
-    cola.imprimir();
-    cout << "Pila: ";
+    cout << "Ingrese el valor VIEJO: ";
+    cin >> viejo;
+    cout << "Ingrese el valor NUEVO: ";
+    cin >> nuevo;
+
+    cout << "Pila Original: ";
     pila.imprimir();
+
+    Pila<int> pilA = reemplazar(pila, nuevo, viejo);
+
+    cout << "Pila Modificada: ";
+    for (int i = 0; i < n; ++i) {
+        cout <<  pilA.pop() << endl;
+    }
 
 }
